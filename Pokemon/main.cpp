658edb1dbf7403f9fb6e49a4b4a95677bfa4eb7a
Zebedee-balistique <116427847+Zebedee-balistique@users.noreply.github.com>
2025-08-11 18:02:06 +0200

    Update main.cpp

--- Pokemon/main.cpp
+++ Pokemon/main.cpp
@@ -2,5 +2,10 @@
 
 int main() {
 
+    std::string player_name;
+
+    std::cout << "Enter your name" << '\n';
+    std::cin >> player_name;
+    std::cout << "Great Start " << player_name << ", looks like you have understood the main() function properly now!";
     return 0;
 }
